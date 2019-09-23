b7b270a659d2bd93ba49dbebf5efbe9c4dbc0e0c
s4322286 <declan.wilkinson@uq.net.au>
2019-09-23 21:14:59 +1000

    made a lil animation

--- js/script.js
+++ js/script.js
@@ -12,21 +12,40 @@ function checkScroll() {
 
 
 $(document).ready(function(event) {
+    if (window.location.hash) {
+        
+        var hash = window.location.hash;
+        console.log(hash);
+        $('html, body').animate({
+            scrollTop :  $(hash).offset().top
+        }, 1000);
+    };
+
+    $('a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $(this.hash).offset().top
+        }, 500);
+    });
+
+    
+
     checkScroll();
 
     //scroll to about when clicked
-    $("#primary-nav ul li:nth-of-type(2) a").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: $('#about').offset().top }, 1000);
-    });
+    // $("#primary-nav ul li:nth-of-type(2) a").click(function(event) {
+    //     event.preventDefault();
+    //     $("html, body").animate({ scrollTop: $('#about').offset().top }, 1000);
+    // });
     //scroll to contact when clicked
-    $("#primary-nav ul li:nth-of-type(3) a").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: $('#contact').offset().top }, 1000);
-    });
+    // $("#primary-nav ul li:nth-of-type(3) a").click(function(event) {
+    //     event.preventDefault();
+    //     $("html, body").animate({ scrollTop: $('#contact').offset().top }, 1000);
+    // });
+    //scroll to top
     $("#top-button").click(function(event) {
         event.preventDefault();
-        $("html, body").animate({ scrollTop: $('header').offset().top }, 1000);
+        $("html, body").animate({ scrollTop: $('header').offset().top }, 500);
     })
     
 });
